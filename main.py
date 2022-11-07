3677cf050e80764a68d29145bbb91756a164754a
hb1136 <77855608+han191299@users.noreply.github.com>
2022-11-07 13:25:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-hello world
-Its me
+import mysql.connector
+import sys
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="methods"
+    )
+
+    print("Successful connection.")
+
+except:
+    print("Failed connection.")
+    sys.exit()
+
+
+
+cursor = connection.cursor()
+
